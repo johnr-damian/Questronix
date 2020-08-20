da0614f270dad8e8e260bb45e9eca350483b2a20
John Rhey Damian <johnr.damian98@gmail.com>
2020-08-21 00:22:09 +0800

    Minimum Working Application (CRUD).

--- controllers/itemcontroller.js
+++ controllers/itemcontroller.js
@@ -2,15 +2,57 @@ const ITEMMODEL = require("../models/itemmodel.js");
 
 exports.CreateNewItem = (request, response) => {
     console.log("Controller -> CreateNewItem");
+    console.log(request.body);
+
+    //Create the item
+    ITEMMODEL.CreateNewItem(request.body.Name, request.body.Quantity, request.body.Amount, (error, results) => {
+        if(error)
+        {
+            console.log(error);
+            response.status(500).render("Uh oh! An error occured!");
+        }
+        else
+        {
+            console.log("Controller -> CreateNewItem END");
+            response.sendStatus(200);
+        }
+    });
 };
 
 exports.UpdateExistingItem = (request, response) => {
     console.log("Controller -> UpdateExistingItem");
-    console.log(request);
+    
+    //Update the item
+    ITEMMODEL.UpdateExistingItem(request.body.ItemID, request.body.Name, request.body.Quantity, request.body.Amount, (error, results) => {
+        if(error)
+        {
+            console.log(error);
+            response.status(500).render("Uh oh! An error occured!");
+        }
+        else
+        {
+            console.log("Controller -> UpdateExistingItem END");
+            response.redirect("/");
+        }
+    });
 };
 
 exports.DeleteExistingItem = (request, response) => {
     console.log("Controller -> DeleteExistingItem");
+
+    //Delete the item
+    ITEMMODEL.DeleteExistingItem(request.body.ItemID, (error, results) => {
+        if(error)
+        {
+            console.log(error);
+            response.status(500).render("Uh oh! An error occured!");
+        }
+        else
+        {
+            console.log("Controller -> DeleteExistingItem END");
+            response.sendStatus(200);
+        }
+    });
 };
 
 exports.ShowItems = (request, response) => {
@@ -26,6 +68,7 @@ exports.ShowItems = (request, response) => {
         }
         else
         {
+            console.log(results);
             if(ItemID === -1)
                 response.render("index", {
                     items: results[0]
@@ -34,6 +77,8 @@ exports.ShowItems = (request, response) => {
                 response.render("pages/view", {
                     item: results[0][0]
                 });
+
+            console.log("Controller -> ShowItems END");
         }
     });
 };
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const EXPRESS = require("express");
 const APPLICATION = EXPRESS();
+const BODYPARSER = require("body-parser");
 APPLICATION.set("view engine", "ejs");
+APPLICATION.use(BODYPARSER.urlencoded({
+    extended: true
+}));
+APPLICATION.use(BODYPARSER.json());
 
 const ITEMROUTES = require("./routes/itemroutes.js");
 APPLICATION.use(ITEMROUTES);
